e02f0090dcede2ce927f459685eaa384f16810e3
Ben Dodson <ben@bendodson.com>
2015-06-23 15:59:33 +0100

    First code commit

---
+++ NSDate-Extensions.swift
@@ -0,0 +1,38 @@
+extension NSDate {
+    func since() -> String {
+        let seconds = abs(NSDate().timeIntervalSince1970 - self.timeIntervalSince1970)
+        if seconds <= 120 {
+            return "just now"
+        }
+        let minutes = Int(floor(seconds / 60))
+        if minutes <= 60 {
+            return "\(minutes) mins ago"
+        }
+        let hours = minutes / 60
+        if hours <= 24 {
+            return "\(hours) hrs ago"
+        }
+        if hours <= 48 {
+            return "yesterday"
+        }
+        let days = hours / 24
+        if days <= 30 {
+            return "\(days) days ago"
+        }
+        if days <= 14 {
+            return "last week"
+        }
+        let months = days / 30
+        if months == 1 {
+            return "last month"
+        }
+        if months <= 12 {
+            return "\(months) months ago"
+        }
+        let years = months / 12
+        if years == 1 {
+            return "last year"
+        }
+        return "\(years) years ago"
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# nsdate-since-swift
-Human readable NSDate time difference in Swift (i.e. "2 hours ago")
+# Since, an NSDate Extension
+Adds a simple method called since() to NSDate so you can get a human readable version of the time difference between then and now i.e. "2 hours ago", "just now", or "last week"
+
+To use, just include NSDate-Extensions.swift in your project and call .since() on any NSDate object.
